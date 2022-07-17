2d9e11015d0ee4c5b51c61ed552f5e8cceb64805
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-17 12:27:31 +0430

    detail added

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from pyparsing import Optional
-from requests import request
+from django.views.generic.detail import DetailView
 from .models import Task 
-from base.models import Task
 
 class TaskList(ListView):
     model = Task
+    
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name: 'base/task.html'
+
